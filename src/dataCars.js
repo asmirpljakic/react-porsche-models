1151bb6307356c445f16c099093171873db08732
Asmir Pljakic <92116999+Topgallantt@users.noreply.github.com>
2023-04-06 09:20:14 +0200

    Add one car

--- src/dataCars.js
+++ src/dataCars.js
@@ -1,4 +1,4 @@
-export const cars = [
+export const dataCars = [
   {
     id: 1,
     img: 'https://files.porsche.com/filestore/image/multimedia/none/911-tu-modelimage-sideshot/thumbwhite/fe21baca-833c-11eb-80d3-005056bbdc38;sD;twebp/porsche-thumbwhite.webp',
@@ -17,13 +17,12 @@ export const cars = [
     img: 'https://files.porsche.com/filestore/image/multimedia/none/e3-tgtc-modelimage-sideshot/thumbwhite/aba76a99-c9e7-11eb-80d7-005056bbdc38;twebp/porsche-thumbwhite.webp',
     model: 'Cayenne Turbo GT',
     owner: 'Novak Djokovic',
-    
   },
+
   {
     id: 4,
-    img: 'https://files.porsche.com/filestore/image/multimedia/none/e3-tgtc-modelimage-sideshot/thumbwhite/aba76a99-c9e7-11eb-80d7-005056bbdc38;twebp/porsche-thumbwhite.webp',
-    model: 'Cayenne Turbo GT',
-    owner: 'Novak Djokovic',
-    
+    img: 'https://files.porsche.com/filestore/image/multimedia/none/982-718-bo-modelimage-sideshot/thumbwhite/85f01af2-a8ce-11eb-80d5-005056bbdc38;sO;twebp/porsche-thumbwhite.webp',
+    model: '718 Boxster',
+    owner: 'Semir Pljakic',
   },
 ];
