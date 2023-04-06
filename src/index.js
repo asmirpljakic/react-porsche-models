ada7c9e07215d4619d5d19183095bf3f4ab0d846
Asmir Pljakic <92116999+Topgallantt@users.noreply.github.com>
2023-04-06 09:20:56 +0200

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import './index.css';
-import { cars } from './cars';
 import Car from './Car';
 
 const rootElement = document.getElementById('root');
@@ -10,9 +9,7 @@ const root = createRoot(rootElement);
 function CarList() {
   return (
     <section className='carlist'>
-      {cars.map(car => {
-        return <Car key={car.id} {...car}></Car>;
-      })}
+      <Car />
     </section>
   );
 }
